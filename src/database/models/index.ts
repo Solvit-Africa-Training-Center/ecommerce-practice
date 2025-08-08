e6a08895d9e4ea2a878adbfa800668d878225939
unknown <jbniyikiza20@gmail.com>
2025-08-08 13:03:48 +0200

    add user register

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -1,10 +1,13 @@
 import { Sequelize } from "sequelize";
 import { UserModal, User } from "./Users";
+import { RoleModel, Role } from "./Roles";
 interface Modals {
   User: typeof User;
+  Role: typeof Role;
 }
 export const AllModal = (sequelize: Sequelize): Modals => {
   return {
     User: UserModal(sequelize),
+    Role: RoleModel(sequelize),
   };
 };
--- src/database/seeders/20250808080448-Roles.js
+++ src/database/seeders/20250808080448-Roles.js
@@ -1,25 +1,49 @@
 "use strict";
 
+const { v4: uuidv4 } = require("uuid");
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-     */
+    // Check if any roles already exist
+    const roles = await queryInterface.sequelize.query(
+      `SELECT COUNT(*) as count FROM roles;`,
+      { type: Sequelize.QueryTypes.SELECT },
+    );
+
+    if (roles[0].count > 0) {
+      console.log("Roles already seeded. Skipping...");
+      return;
+    }
+
+    // Proceed with seeding roles
+    await queryInterface.bulkInsert("roles", [
+      {
+        id: uuidv4(),
+        name: "admin",
+        description: "Platform administrator with full access",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: "customer",
+        description: "Regular shopper with access to browse and purchase",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: uuidv4(),
+        name: "seller",
+        description:
+          "Vendor with permission to manage their products and orders",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete("roles", null, {});
   },
 };
