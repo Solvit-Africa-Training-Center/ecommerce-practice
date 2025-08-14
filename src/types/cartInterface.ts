49ba74b777e3056513386db5701f57d8a6a3d045
GANZA Clebert Sabin <clebertsabinganza@gmail.com>
2025-08-14 19:02:25 +0200

    full shopping carts

---
+++ src/types/cartInterface.ts
@@ -0,0 +1,29 @@
+import { Request } from 'express';
+import { CartItem } from '../database/models/cartItemModel';
+import { CartAttributes } from '../database/models/cartModel';
+
+export interface ICartWithItems extends CartAttributes {
+  items?: CartItem[]; 
+}
+
+
+export interface ICartItem {
+  id?: string;
+  cartId: string;
+  productId: string;
+  quantity: number;
+  price: number;
+  totalprice?: number;
+}
+
+export interface ICart {
+  id?: string;
+  userId: string;
+  items?: ICartItem[];
+  totalPrice?: number;
+}
+
+
+export interface IRequestUser extends Request {
+  user?: { id: string; role?: string };
+}
